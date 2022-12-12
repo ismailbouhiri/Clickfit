a8273c16316afde2b614bef967eb05eb562b44e6
ismail bouhiri <amirisma8@gmail.com>
2022-12-12 20:49:45 +0100

    headerSection: add some animation about the event annonce (text)

--- js/app.js
+++ js/app.js
@@ -8,4 +8,14 @@ $(document).ready(() => {
             $('.sticky').removeClass('stickyAdd')
         }
     })
+    let typed = new Typed(".event", {
+        strings: ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, facere.'],
+        showCursor: false,
+        smartBackspace: true,
+        typeSpeed: 20,
+        backSpeed: 20,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 1000
+    });
 })
